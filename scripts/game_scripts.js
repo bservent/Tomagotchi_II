032c022840fb3961d9c64aa96cc919d2d11d46be
bservent <bservent@gmail.com>
2022-04-15 16:38:46 -0700

    variables added for cursor paddle

---
+++ scripts/game_scripts.js
@@ -0,0 +1,8 @@
+var canvas = document.getElementById("my_canvas");
+var x = canvas.width/2;
+var y = canvas.height-30;
+var paddleHeight = 10;
+var paddleWidth = 75;
+var paddleX = (canvas.width-paddleWidth) / 2;
+var rightPressed = false;
+var leftPressed = false;
